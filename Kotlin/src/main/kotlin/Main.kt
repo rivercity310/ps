4e0a0a9a758b1e556a4d22ebb3d793a31b46ef61
seungsu <h970126@gmail.com>
2022-11-27 23:58:05 +0900

    kt

--- Kotlin/src/main/kotlin/Main.kt
+++ Kotlin/src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 import beginner.a_Sorting.*
 import beginner.c_BinarySearch.*
+import beginner.d_GraphSearch.*
 
 fun main(args: Array<String>) {
-    boj_2110()
+    quick_sort2()
 }
---
+++ Kotlin/src/main/kotlin/beginner/a_Sorting/quick_sort2.kt
@@ -0,0 +1,44 @@
+package beginner.a_Sorting
+
+// 제자리 정렬을 사용해 메모리 효율적
+
+fun partition(arr: IntArray, start: Int, end: Int): Int {
+    var left = start
+    var right = end
+    val pivot = arr[(left + right) / 2]
+
+    while (left <= right) {
+        while (arr[left] < pivot) left++
+        while (arr[right] > pivot) right--
+
+        if (left <= right) {
+            arr[left] = arr[right].also { arr[right] = arr[left] }
+            left++
+            right--
+        }
+    }
+
+    return left
+}
+
+fun solve(arr: IntArray, left: Int = 0, right: Int = arr.lastIndex) {
+    val index = partition(arr, left, right)
+
+    if (left < index - 1)
+        solve(arr, left, index - 1)
+    if (index < right)
+        solve(arr, index, right)
+}
+
+fun quick_sort2() = with(System.`in`.bufferedReader()) {
+    val arr = readLine()
+        .split(" ")
+        .map { it.toInt() }
+        .toIntArray()
+
+    val sorted_arr = solve(arr)
+
+    println("[ 정렬 완료 ]")
+    for (k in sorted_arr)
+        print("$k ")
+}
