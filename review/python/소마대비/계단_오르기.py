ff5ad55d8e4e36a2009812fe2449205c0af67a15
seungsu <h970126@gmail.com>
2023-02-07 22:49:54 +0900

    bfs

--- review/python/소마대비/계단_오르기.py
+++ review/python/소마대비/계단_오르기.py
@@ -1,27 +1,23 @@
 import sys
 
 n = int(sys.stdin.readline())
-stair = list()
+stair = list([0])
 
 for i in range(n):
     stair.append(int(sys.stdin.readline()))
 
 if n == 1:
-    print(stair[0])
+    print(stair[1])
 elif n == 2:
-    print(stair[0] + stair[1])
+    print(stair[1] + stair[2])
 else:
-    # n == 3: (1, 3), (2, 3)       stair[n] + max(dp[n - 1], dp[n - 2])
-    # n == 4: (1, 2, 4), (1, 3, 4)
-    # n == 5: (1, 3, 5), (1, 2, 4, 5)
-
     dp = [0] * (n + 1)
     dp[1] = stair[1]
     dp[2] = dp[1] + stair[2]
     dp[3] = max(dp[1], stair[2]) + stair[3]
 
     for i in range(4, n + 1):
-        pass
+        dp[i] = max(dp[i - 2], dp[i - 3] + stair[i - 1]) + stair[i]
 
     print(dp[n])
 
--- review/python/소마대비/숨바꼭질.py
+++ review/python/소마대비/숨바꼭질.py
@@ -1,33 +1,23 @@
+from collections import deque
+
 n, k = map(int, input().split())
 
-if n == k:
-    print(0)
+limit = 100000
+q = deque()
+distance = [0] * (limit + 1)
+q.append(n)
 
-elif abs(n - k) == 1:
-    print(1)
+while q:
+    now = q.popleft()
 
-else:
-    # k가 항상 n보다 크도록
-    if n > k:
-        n, k = k, n
+    if now == k:
+        print(distance[k])
+        break
 
-    inf = int(1e9)
-    dp = [inf] * (k * 2 + 1)
-    dp[n] = 0
+    for next in (now - 1, now + 1, now * 2):
+        if next < 0 or next > limit:
+            continue
 
-    for i in range(n - 1, n - 10, -1):
-        dp[n] = dp[n + 1] + 1
-
-    for i in range(n, k * 2):
-        while i * 2 < k * 2:
-            print(f'dp[{i * 2}] -> {dp[i] + 1}')
-            dp[i * 2] = dp[i] + 1
-
-    for i in range(n + 1, k * 2):
-        if i % 2 == 0:
-            dp[i] = min(dp[i // 2], dp[i - 1], dp[i + 1]) + 1
-        else:
-            dp[i] = min(dp[i - 1], dp[i + 1]) + 1
-
-    print(dp[k])
-    print(dp)
+        if not distance[next]:
+            distance[next] = distance[now] + 1
+            q.append(next)
