be3c0505fc5909053483f377a9cfd93165190fdc
seungsu <h970126@gmail.com>
2023-02-24 02:18:30 +0900

    2022/02/24

---
+++ review/java/src/main/java/org/example/kit/Greedy/PRO42860.java
@@ -0,0 +1,15 @@
+package org.example.kit.Greedy;
+
+public class PRO42860 {
+    static char[] alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+
+    public static int solution(String name) {
+        int cnt = 0;
+        char[] char_name = name.toCharArray();
+
+
+        return cnt;
+    }
+
+
+}
