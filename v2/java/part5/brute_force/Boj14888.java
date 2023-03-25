1a54ed4764ac6c81a751d3932ebd9146bc4fcb21
seungsu <h970126@gmail.com>
2023-03-25 23:34:24 +0900

    [Part5] : BackTracking

--- v2/java/part5/brute_force/Boj14888.java
+++ v2/java/part5/brute_force/Boj14888.java
@@ -7,24 +7,24 @@ import java.util.Arrays;
 
 public class Boj14888 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static int[] nums, ops;
     static int N;
     static int max = Integer.MIN_VALUE;
     static int min = Integer.MAX_VALUE;
+    static int[] nums, ops;
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
         nums = Arrays.stream(br.readLine().split("\\s")).mapToInt(Integer::parseInt).toArray();
         ops = Arrays.stream(br.readLine().split("\\s")).mapToInt(Integer::parseInt).toArray();
 
-        solve(1, nums[0]);
+        solve(nums[0], 0);
 
         System.out.println(max);
         System.out.println(min);
     }
 
-    static void solve(int depth, int sum) {
-        if (depth == N) {
+    static void solve(int sum, int depth) {
+        if (depth == N - 1) {
             max = Math.max(max, sum);
             min = Math.min(min, sum);
             return;
@@ -33,16 +33,18 @@ public class Boj14888 {
         for (int i = 0; i < ops.length; i++) {
             if (ops[i] > 0) {
                 ops[i]--;
-
-                switch (i) {
-                    case 0 : solve(depth + 1, sum + nums[depth]); break;
-                    case 1 : solve(depth + 1, sum - nums[depth]); break;
-                    case 2 : solve(depth + 1, sum * nums[depth]); break;
-                    case 3 : solve(depth + 1, sum / nums[depth]); break;
-                }
-
+                solve(calc(i, sum, nums[depth + 1]), depth + 1);
                 ops[i]++;
             }
         }
+    }
+
+    static int calc(int op, int sum, int num) {
+        if (op == 0) sum += num;
+        else if (op == 1) sum -= num;
+        else if (op == 2) sum *= num;
+        else sum /= num;
+
+        return sum;
     }
 }
---
+++ v2/java/part5/brute_force/Boj14889.java
@@ -0,0 +1,52 @@
+package part5.brute_force;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Boj14889 {
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N;
+    static int ans = Integer.MAX_VALUE;
+    static int[][] S;
+    static boolean[] T;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        S = new int[N][N];
+        T = new boolean[N];
+
+        for (int i = 0; i < N; i++) {
+            S[i] = Arrays.stream(br.readLine().split("\\s")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        solve(0, 0);
+        System.out.println(ans);
+    }
+
+    static void solve(int depth, int start) {
+        if (depth == N / 2) {
+            int startScore = 0;
+            int linkScore = 0;
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (T[i] && T[j]) startScore += S[i][j];
+                    if (!T[i] && !T[j]) linkScore += S[i][j];
+                }
+            }
+
+            ans = Math.min(ans, Math.abs(startScore - linkScore));
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            if (!T[i]) {
+                T[i] = true;
+                solve(depth + 1, i);
+                T[i] = false;
+            }
+        }
+    }
+}
