a6e731f53f3b00acded4af7ac271ebb880a0d114
황승수 (Seungsu) <h970126@gmail.com>
2023-04-10 14:52:39 +0900

    카드

---
+++ v2/java/part5/sorting/Boj11652.java
@@ -0,0 +1,38 @@
+package part5.sorting;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Boj11652 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N;
+    static Map<Long, Integer> m;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        m = new TreeMap<>();
+
+        for (int i = 0; i < N; i++) {
+            long k = Long.parseLong(br.readLine());
+            m.merge(k, 1, Integer::sum);
+        }
+
+        int max = 0;
+        long ans = 0;
+
+        for (Long l : m.keySet()) {
+            int a = m.get(l);
+            if (a > max) {
+                max = a;
+                ans = l;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
